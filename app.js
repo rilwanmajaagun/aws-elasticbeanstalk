1891c07e6d2ac5967feec23d7385dc1b727d2801
kollyd <majaagunoyinkolade@gmail.com>
2021-03-04 07:42:04 +0100

    deploying to aws elsatic beanstalk

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`application listening on port ${port}`)
+})
+
+
+app.get('/', (req, res) => {
+  return res.send('!!!! welcome to the home page')
+})
